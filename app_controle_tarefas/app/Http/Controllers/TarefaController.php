41824f3103972a2783517325c2f8840654f17914
Matheus <nmmatheus@hotmail.com>
2025-05-25 14:47:18 -0300

    feat: validação de usuário
    Validando se a tarefa pertence ao usuário antes de editar

--- app_controle_tarefas/app/Http/Controllers/TarefaController.php
+++ app_controle_tarefas/app/Http/Controllers/TarefaController.php
@@ -81,7 +81,11 @@ class TarefaController extends Controller
      */
     public function edit(Tarefa $tarefa)
     {
-        return view('tarefa.edit', ['tarefa' => $tarefa]);
+        $user_id = auth()->user()->id;
+        if ($tarefa->user_id == $user_id) {
+            return view('tarefa.edit', ['tarefa' => $tarefa]);
+        }
+        return view('acesso-negado');
     }
 
     /**
@@ -93,6 +97,11 @@ class TarefaController extends Controller
      */
     public function update(Request $request, Tarefa $tarefa)
     {
+        $user_id = auth()->user()->id;
+        if ($tarefa->user_id != $user_id) {
+            return view('acesso-negado');
+        }
+        
         $request->validate([
             'tarefa' => 'required|min:3',
             'data_limite' => 'required|date'
