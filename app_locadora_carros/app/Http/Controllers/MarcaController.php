6aee962feb0d00767639cccd51a2846b3b66e1f9
Matheus <nmmatheus@hotmail.com>
2025-08-19 22:28:53 -0300

    ci: Injetando a instaâância do Model no Controller e Typing Hinting

--- app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -8,13 +8,24 @@ use Illuminate\Http\Request;
 class MarcaController extends Controller
 {
     /**
+     * The Marca model instance.
+     *
+     * @var \App\Models\Marca
+     */
+    protected $marca;
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
     public function index()
     {
-        $marcas = Marca::all();
+        // $marcas = Marca::all();
+        $marcas = $this->marca->all();
         return $marcas;
     }
 
@@ -26,42 +37,79 @@ class MarcaController extends Controller
      */
     public function store(Request $request)
     {
-        $marca = Marca::create($request->all());
+        // $marca = Marca::create($request->all());
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
+    // /**
+    //  * Display the specified resource.
+    //  *
+    //  * @param  \App\Models\Marca  $marca
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function show(Marca $marca)
+    // {
+    //     return $marca;
+    // }
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
+        $marca = $this->marca->findOrFail($id);
         return $marca;
     }
 
+    // /**
+    //  * Update the specified resource in storage.
+    //  *
+    //  * @param  \Illuminate\Http\Request  $request
+    //  * @param  \App\Models\Marca  $marca
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function update(Request $request, Marca $marca)
+    // {
+    //     $marca->update($request->all());
+    //     return $marca;
+    // }
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
+        $marca = $this->marca->findOrFail($id);
         $marca->update($request->all());
         return $marca;
     }
 
+    // /**
+    //  * Remove the specified resource from storage.
+    //  *
+    //  * @param  \App\Models\Marca  $marca
+    //  * @return \Illuminate\Http\Response
+    //  */
+    // public function destroy(Marca $marca)
+    // {
+    //     $marca->delete();
+    //     return ['msg' => 'Marca deletada com sucesso!'];
+    // }
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
+        $marca = $this->marca->findOrFail($id);
         $marca->delete();
         return ['msg' => 'Marca deletada com sucesso!'];
     }
